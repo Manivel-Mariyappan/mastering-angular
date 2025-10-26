925f7c9c8a498f575ab6bbf4c643c08735a10184
Manivel <maniveluideveloper@gmail.com>
2025-10-26 22:57:48 +0530

    Deve

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./Layout/main/main').then(m => m.Main),
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            },
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard)
+            },
+            {
+                path: 'forms',
+                loadComponent: () => import('./forms/forms').then(m => m.Forms)
+            }
+        ]
+    }
+
+];
